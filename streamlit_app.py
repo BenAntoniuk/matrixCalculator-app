796107cf8b9dd3631abd945ed11a449dabfdc50e
Benjamin Antoniuk <156739340+BenAntoniuk@users.noreply.github.com>
2025-09-24 12:20:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Matrix Calculator")
 st.write(
     "Block of code [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
